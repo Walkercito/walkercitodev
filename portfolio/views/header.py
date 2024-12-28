3a2efb3112997ae8d085bc9a5388e0c326350ebd
Walkercito <yoanky2005@gmail.com>
2024-12-27 23:43:00 -0500

    almost

--- portfolio/views/header.py
+++ portfolio/views/header.py
@@ -8,34 +8,12 @@ from portfolio.styles.styles import Size
 
 def header(data: Data) -> rx.Component:
     return rx.vstack(
-        rx.hstack(
-            rx.avatar(
-                src=data.avatar,
-                size=Size.BIG.value
-            ),
-            rx.vstack(
-                heading(data.name, True),
-                heading(data.skill),
-                rx.text(
-                    rx.icon("map-pin"),
-                    data.location,
-                    display="inherit"
-                ),
-                spacing=Size.SMALL.value,
-            ),
-            spacing=Size.DEFAULT.value,
-            width="100%",
-        ),
         rx.mobile_only(
             rx.vstack(
-                media(data.media),
-                spacing=Size.SMALL.value,
-                align_items="center"
-            ),
-            width="100%"
-        ),
-        rx.tablet_and_desktop(
-            rx.hstack(
+                rx.avatar(
+                    src=data.avatar,
+                    size=Size.BIG.value
+                ),
                 rx.vstack(
                     heading(data.name, True),
                     heading(data.skill),
@@ -44,12 +22,34 @@ def header(data: Data) -> rx.Component:
                         data.location,
                         display="inherit"
                     ),
+                    media(data.media),
                     spacing=Size.SMALL.value,
                 ),
-                media(data.media),
                 spacing=Size.DEFAULT.value,
+                align_items="center"
             ),
             width="100%"
+        ),
+        rx.tablet_and_desktop(
+            rx.hstack(
+                rx.avatar(
+                    src=data.avatar,
+                    size=Size.BIG.value
+                ),
+                rx.vstack(
+                    heading(data.name, True),
+                    heading(data.skill),
+                    rx.text(
+                        rx.icon("map-pin"),
+                        data.location,
+                        display="inherit"
+                    ),
+                    media(data.media),
+                    spacing=Size.SMALL.value,
+                ),
+                spacing=Size.DEFAULT.value,
+                width="100%"
+            )
         ),
         spacing=Size.DEFAULT.value,
         align_items="stretch",
