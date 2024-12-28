56f94c58e1ed71ba32ec570870023ab909d018ab
Walkercito <yoanky2005@gmail.com>
2024-12-27 23:19:27 -0500

    Update header.py

--- portfolio/views/header.py
+++ portfolio/views/header.py
@@ -7,21 +7,38 @@ from portfolio.styles.styles import Size
 
 
 def header(data: Data) -> rx.Component:
-    return rx.hstack(
-        rx.avatar(
-            src = data.avatar,
-            size = Size.BIG.value
-        ),
-        rx.vstack(
-            heading(data.name, True),
-            heading(data.skill),
-            rx.text(
-                rx.icon("map-pin"),
-                data.location,
-                display="inherit"
+    return rx.vstack(
+        rx.hstack(
+            rx.avatar(
+                src = data.avatar,
+                size = Size.BIG.value
             ),
+            rx.vstack(
+                heading(data.name, True),
+                heading(data.skill),
+                rx.text(
+                    rx.icon("map-pin"),
+                    data.location,
+                    display = "inherit"
+                ),
+                spacing = Size.SMALL.value,
+            ),
+            spacing = Size.DEFAULT.value,
+            width = "100%",
+        ),
+        rx.mobile_only(
+            rx.hstack(
+                media(data.media),
+                spacing = Size.SMALL.value,
+                justify = "center"
+            ),
+            width = "100%"
+        ),
+        rx.tablet_and_desktop(
             media(data.media),
             spacing = Size.SMALL.value,
+            width = "100%",
         ),
         spacing = Size.DEFAULT.value,
+        align_items = "stretch",
     )
